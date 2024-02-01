954900f73be8765e51c7beea74fe25faea9a567e
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 15:29:03 +0300

    fix paths

--- src/utils/fb2_converter/convert.py
+++ src/utils/fb2_converter/convert.py
@@ -19,5 +19,6 @@ def convert(fb_abs_path: str | Path, output_dir: str | Path) -> None:
     fb2c_path = fb2c
     conf_path = "src/utils/fb2_converter/fb2c_win64/fb2converter/configuration.toml"
 
+
     command = f"{fb2c_path} -c {conf_path} convert --to epub {fb_abs_path} {output_dir}"
     result = subprocess.run(command)
