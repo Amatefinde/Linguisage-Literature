6725799dc8e61825e7c2d0bf569e4c1e4181869f
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 16:02:13 +0300

    add all

--- src/utils/fb2_converter/convert.py
+++ src/utils/fb2_converter/convert.py
@@ -10,14 +10,19 @@ def convert(fb_abs_path: str | Path, output_dir: str | Path) -> None:
     system_platform = platform.system()
 
     if system_platform == "Linux":
-        fb2c = Path("src/utils/fb2_converter/fb2c")
+        fb2c_path = join(
+            settings.base_dir,
+            "src/utils/fb2_converter/fb2c",
+        )
     elif system_platform == "Windows":
-        fb2c = Path("src/utils/fb2_converter/fb2c_win64/fb2converter/fb2c.exe")
+        fb2c_path = join(
+            settings.base_dir,
+            "src/utils/fb2_converter/fb2c_win64/fb2converter/fb2c.exe",
+        )
     else:
         raise OSError("Unsupported platform")
 
-    fb2c_path = fb2c
     conf_path = "src/utils/fb2_converter/fb2c_win64/fb2converter/configuration.toml"
 
     command = f"{fb2c_path} -c {conf_path} convert --to epub {fb_abs_path} {output_dir}"
-    result = subprocess.run(command, shell=True)
+    subprocess.run(command, shell=True)
