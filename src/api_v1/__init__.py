73000b0342efb94d9b668b73aa9856e9ff27126c
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 14:24:20 +0300

    update imports

--- src/api_v1/__init__.py
+++ src/api_v1/__init__.py
@@ -4,5 +4,5 @@ from src.api_v1.books.views import router as books_router
 from src.core import settings
 
 router = APIRouter(prefix=settings.api_v1_prefix)
-router.include_router(documents_router)
 router.include_router(books_router)
+# router.include_router(documents_router)
---
+++ src/utils/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["make_static_folders", "extract_cover_from_epub"]
+
+
+from .epub_cover import extract_cover_from_epub
+from .make_static_folders import make_static_folders
