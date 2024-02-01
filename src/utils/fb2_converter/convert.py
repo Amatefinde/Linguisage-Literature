61bbb7e80805aaf975dbec55be53ca24c1602b66
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 16:04:46 +0300

    fix path

--- src/utils/fb2_converter/convert.py
+++ src/utils/fb2_converter/convert.py
@@ -10,15 +10,9 @@ def convert(fb_abs_path: str | Path, output_dir: str | Path) -> None:
     system_platform = platform.system()
 
     if system_platform == "Linux":
-        fb2c_path = Path(
-            settings.base_dir,
-            "src/utils/fb2_converter/fb2c",
-        )
+        fb2c_path = "src/utils/fb2_converter/fb2c"
     elif system_platform == "Windows":
-        fb2c_path = Path(
-            settings.base_dir,
-            "src/utils/fb2_converter/fb2c_win64/fb2converter/fb2c.exe",
-        )
+        fb2c_path = "src/utils/fb2_converter/fb2c_win64/fb2converter/fb2c.exe"
     else:
         raise OSError("Unsupported platform")
 
