ff2b37a94461d15cf125169c8cd7e08e14013569
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 15:36:27 +0300

    fix paths

--- test_converter.py
+++ test_converter.py
@@ -3,6 +3,6 @@ from os.path import join
 from src.utils.fb2_converter import convert
 from src.core import settings
 
-fb = join(settings.base_dir, r"src/utils/voina-i-mir.fb2")
-epub = join(settings.base_dir, r"sandbox")
+fb = "src/utils/voina-i-mir.fb2"
+epub = "sandbox"
 convert(fb, epub)
