3e2aa00af8974f19568e32754307cf087c2469b7
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-01 14:45:11 +0300

    upd

---
+++ test_converter.py
@@ -0,0 +1,8 @@
+from os.path import join
+
+from src.utils.fb2_converter import convert
+from src.core import settings
+
+fb = join(settings.base_dir, r"src\utils\voina-i-mir.fb2")
+epub = join(settings.base_dir, r"sandbox")
+convert(fb, epub)
