c5d37abb1147ac7f8d01c2ca248422b7bcebb8a2
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-02 09:11:52 +0300

    method add_book separated on add_epub_book and save_fb2_book

--- src/api_v1/books/crud.py
+++ src/api_v1/books/crud.py
@@ -1,29 +1,58 @@
 from fastapi import UploadFile
-
+from src.background_tasks import handle_fb2
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import SPatchRequest
-from .utils import _save_book_to_disk, _save_book_cover_to_disk, _remove_book_from_disk
+from src.api_v1.books.book_storage import (
+    _save_epub_to_disk,
+    _save_book_cover_to_disk,
+    _remove_book_from_disk,
+    _save_fb2_in_tmp,
+)
 from src.core.database.models import LiteratureEpub
 
 
-async def add_book(
+async def add_epub_book(
     session: AsyncSession,
     book: UploadFile,
     filename: str,
 ):
-    disk_file_name = _save_book_to_disk(book)
+
+    disk_file_name = _save_epub_to_disk(book)
     disk_file_cover_name = _save_book_cover_to_disk(book)
 
     db_epub = LiteratureEpub(
         title=filename,
         original_file=disk_file_name,
         cover=disk_file_cover_name,
+        is_processed=True,
     )
 
     session.add(db_epub)
     await session.commit()
     await session.refresh(db_epub)
+    return db_epub
+
+
+async def add_fb2_book(
+    session: AsyncSession,
+    book: UploadFile,
+    user_chosen_filename: str,
+):
+    filename_without_extension = _save_fb2_in_tmp(book)
+
+    db_epub = LiteratureEpub(
+        title=user_chosen_filename,
+        original_file=f"{filename_without_extension}.epub",
+        is_processed=False,
+    )
+
+    session.add(db_epub)
+    await session.commit()
+    await session.refresh(db_epub)
+
+    handle_fb2.delay(filename_without_extension, db_epub.id)
+
     return db_epub
 
 
