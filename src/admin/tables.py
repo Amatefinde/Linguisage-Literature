483ea401d50d9f689c700c30d34d0ff80f115fdb
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-02 10:00:26 +0300

    add LiteratureEpub model for admin

---
+++ src/admin/tables.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from sqladmin import ModelView
+from starlette.requests import Request
+
+from src.api_v1.books.crud import get_book, delete_book
+from src.core.database.models import LiteratureEpub
+from src.core.database import db_helper
+
+
+class AdminLiteratureEpub(ModelView, model=LiteratureEpub):
+    column_list = [
+        LiteratureEpub.id,
+        LiteratureEpub.title,
+        LiteratureEpub.created_at,
+        LiteratureEpub.last_opened_at,
+    ]
+
+    async def delete_model(self, request: Request, pk: Any) -> None:
+        async with db_helper.async_session_factory() as session:
+            db_book = await get_book(session, int(pk))
+            await delete_book(session, db_book)
