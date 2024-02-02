deb5d1b08f8d794e3586badc0fe8a9f4534ec263
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2024-02-02 11:58:55 +0300

    add logging

--- src/utils/make_static_folders.py
+++ src/utils/make_static_folders.py
@@ -1,21 +1,23 @@
 from os import mkdir
 from os.path import isdir, join
-
 from loguru import logger
 
 from src.core import settings
 
 
 def make_static_folders() -> None:
-    logger.debug("Making static folder")
     if not isdir(settings.abs_static_path):
         mkdir(settings.abs_static_path)
+        logger.info("static dir created")
 
     if not isdir(settings.epub_dir):
         mkdir(settings.epub_dir)
+        logger.info("epub dir created")
 
     if not isdir(settings.epub_cover_dir):
         mkdir(settings.epub_cover_dir)
+        logger.info("epub_cover dir created")
 
     if not isdir(settings.fb2_tmp):
         mkdir(settings.fb2_tmp)
+        logger.info("fb2_tmp dir created")
