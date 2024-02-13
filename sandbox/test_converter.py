8fe704b684fa74d03fc4c7bd3f263cd9cbad5cb0
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-02-13 14:48:08 +0300

    removed

--- sandbox/test_converter.py
+++
@@ -1,8 +0,0 @@
-from os.path import join
-
-from src.utils.fb2_converter import convert
-from src.core import settings
-
-fb = join(settings.base_dir, r"src/utils/voina-i-mir.fb2")
-epub = join(settings.base_dir, r"sandbox")
-convert(fb, epub)
