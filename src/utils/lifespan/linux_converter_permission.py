53d070d5e11c11ccb0cd64b24ac1a8008b392311
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-02-15 11:02:43 +0300

    fix linux file permission

--- src/utils/lifespan/linux_converter_permission.py
+++ src/utils/lifespan/linux_converter_permission.py
@@ -7,6 +7,6 @@ def give_permission():
     os_name = platform.system()
     if os_name == "Linux":
         file_path = "src/utils/fb2_converter/fb2c"
-        chmod_command = ["chmod", "+x", file_path]
+        chmod_command = ["chmod", "a+x", file_path]
         subprocess.run(chmod_command)
         logger.info("linux permission given")
