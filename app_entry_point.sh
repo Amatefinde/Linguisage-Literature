e4e4cd83172a3a439af0dbcac676a96e690039bc
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-04-02 17:18:11 +0300

    update domain for letsencrypt certs

--- app_entry_point.sh
+++ app_entry_point.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 
-uvicorn main:app --host 0.0.0.0 --port 9200 --ssl-certfile /literature/letsencrypt/live/linguisage.ru/fullchain.pem --ssl-keyfile /literature/letsencrypt/live/linguisage.ru/privkey.pem
+uvicorn main:app --host 0.0.0.0 --port 9200 --ssl-certfile /literature/letsencrypt/live/api.linguisage.ru/fullchain.pem --ssl-keyfile /literature/letsencrypt/live/api.linguisage.ru/privkey.pem
